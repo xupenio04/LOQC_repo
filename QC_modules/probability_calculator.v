0b1d7e9de772b3aed579cc25b1bd0606090d5003
pvsc <pvsc@hwc18.cin.ufpe.br>
2026-04-15 13:32:39 -0300

    probability module

--- QC_modules/probability_calculator.v
+++ QC_modules/probability_calculator.v
@@ -1,18 +1,45 @@
+module probability_calculator #( NUM_QUBITS = 1)(
+    input wire [7:0] real_part,
+    input wire [7:0] imag_part,
+    output wire [15:0] prob_qubit
 
-module probability_calculator (
-    input signed [15:0] q0_real,
-    input signed [15:0] q0_imag,
-    input signed [15:0] q1_real,
-    input signed [15:0] q1_imag,
-    output wire [31:0] prob0
 );
-
-    wire [31:0] mag0_squared = (q0_real * q0_real) + (q0_imag * q0_imag);
-    wire [31:0] mag1_squared = (q1_real * q1_real) + (q1_imag * q1_imag);
     
-    wire [31:0] total_prob = mag0_squared + mag1_squared;
 
-    wire [63:0] scaled_prob = {32'd0, mag0_squared} << 16;
-    assign prob0 = (total_prob == 0) ? 32'd32768 : scaled_prob / total_prob;
+    wire signed [15:0] square_real;
+    wire signed [15:0] square_imag;
 
+
+    assign prob_qubit = square_real + square_imag; 
+    
+endmodule
+
+
+module ac_mult
+(input signed [7:0] a_real,
+ input signed [7:0] b_real,
+ output signed [15:0] ac
+ );
+
+    localparam SHIFT_BITS=8, MULTIPLY_BITS=1;
+    wire signed [15:0] mult;
+
+   assign mult =
+    (a_real == 8'b00000000 || b_real == 8'b00000000) ? 16'b0 :
+    (a_real == 8'b01111111 && b_real != 8'b10000000 && b_real != 8'b00000000)     ? {b_real-1'b1, 8'b0} :
+    ((a_real == 8'b01111111 && b_real == 8'b10000000))     ? {~a_real + 1'b1, 8'b0}    :
+    ((a_real == 8'b10000000 && b_real == 8'b01111111))     ? {~b_real + 1'b1, 8'b0}    :
+    (a_real == 8'b10000000 && b_real != 8'b00000000 && b_real != 8'b10000000)    ? {~b_real + 1'b1, 8'b0} :
+    (a_real == 8'b10000000 && b_real == 8'b10000000)     ? {~b_real, 8'b0} :
+    (a_real != 8'b01111111 && a_real != 8'b10000000 && b_real == 8'b01111111) ? {a_real-1'b1
+    , 8'b0}:
+    (a_real != 8'b01111111 && a_real != 8'b10000000 && b_real == 8'b10000000 && a_real != 8'b00000000) ? {~a_real + 1'b1, 8'b0}:
+    (a_real[7] == 1'b1 && b_real[7] == 1'b1) ? ({8'b0,(~a_real +1'b1)} * {8'b0,(~b_real +1'b1)})  <<< MULTIPLY_BITS :
+    (a_real[7] == 1'b1 && b_real[7] == 1'b0) ? ~(({8'b0,(~a_real +1'b1)} * {8'b0,b_real})) + 1'b1 <<< MULTIPLY_BITS :
+    (a_real[7] == 1'b0 && b_real[7] == 1'b1) ? ~(({8'b0,a_real} * {8'b0,(~b_real +1'b1)})) + 1'b1 <<< MULTIPLY_BITS :
+    (({8'b0,a_real} * {8'b0,b_real})) <<< MULTIPLY_BITS;
+
+
+
+    assign ac = mult ;
 endmodule
